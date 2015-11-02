e882b152f7fa60fc79370fd0325e7a3efb8d9856
LukeM94 <DRU9HEde>
2015-11-02 13:49:19 +0000

    Initial commit

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+    <div class="login">
+        <h1>Login</h1>
+        <form method="post" action="homepage.html">
+            <p><input type="text" name="login" value="" placeholder="Username"></p>
+            <p><input type="password" name="password" value="" placeholder="Password"></p>
+            <p class="submit"><input type="submit" name="commit" value="Login"></p>
+        </form>
+    </div>
+
+<p>
+    <?php
+
+    ?>
+</p>
+</body>
+</html>
